8bbf28613f30e87d0e2f1c5411436a7a2c35310d
Brianna <megr0006@algonquinlive.com>
2020-12-17 23:57:02 -0500

    completes assignment

---
+++ script.js
@@ -0,0 +1,74 @@
+let settings = {
+  showSeconds: true,
+  showFullDate: false,
+  apiKey: '7dSlUhvTPJW8pkqGxbdjZxB1nGjDL7TfxU7zRPRF',
+  api: 'https://api.nasa.gov/planetary/apod?api_key='
+}
+
+const DateTime = luxon.DateTime
+
+function createClock () {
+  const now = DateTime.local()
+  const day = now.day
+  const month = now.month
+  const year = now.year
+  const hour = now.hour
+  const minutes = now.minute
+  const seconds = now.second
+  let time = '<p>'
+  if (settings.showFullDate) {
+    time += ` ${day}/${month}/${year}/`
+  }
+  time += `${hour}:${minutes}`
+  if (settings.showSeconds) {
+    time += `:${seconds}`
+  }
+  time += '</p>'
+  document.getElementById('clock').innerHTML = time
+}
+setInterval(() => {
+  createClock()
+}, 100)
+
+function changeSettings (event) {
+  event.preventDefault()
+  changeFullDateSettings()
+  changeSecondsSettings()
+  localStorage.setItem('settings', JSON.stringify(settings))
+}
+
+function changeFullDateSettings () {
+  const $fullDate = document.getElementsByName('full')
+
+  settings.showFullDate = $fullDate[0].checked
+}
+
+function changeSecondsSettings () {
+  const $showSettings = document.getElementsByName('seconds')
+
+  settings.showSeconds = $showSettings[0].checked
+}
+
+document.getElementById('submit').addEventListener('click', changeSettings)
+const loadedsettings = localStorage.getItem('settings')
+
+if (loadedsettings !== null) {
+  settings = JSON.parse(loadedsettings)
+}
+
+function setBackground () {
+  const bgUrl = `${settings.api}${settings.apiKey}`
+  fetch(bgUrl)
+    .then(response => response.json())
+    .then(json => {
+      const picture = json.url
+      document.getElementsByTagName("html")[0].style = `height: 100%;background: url('${picture}'); background-repeat: no-repeat; background-size: 100% 100%;`
+    })
+}
+
+
+setBackground()
+
+function show() {
+  document.getElementById('sidebar').classList.toggle('active')
+}
